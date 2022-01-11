b4ae21898a17fcc66ce9f81038235c18e33be04c
mghaby <mark.ghaby71@hotmail.com>
2022-01-11 16:40:35 +1100

    image added, and description added, centering of intro left objects

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,11 +1,12 @@
 import "./intro.css"
+import Me from "../../images/me.png"
 
 const Intro = () => {
     return (
         <div className="Intro">
             <div className="Intro-Left">
                 <div className="Intro-Left-Wrapper">
-                    <h2 className="Intro-Intro">Hello, My name is</h2>
+                    <h2 className="Intro-Intro">Hello, my name is</h2>
                     <h1 className="Intro-Name">Mark Ghaby.</h1>
                     <div className="Intro-Title">
                         <div className="Intro-Title-Wrapper">
@@ -14,13 +15,20 @@ const Intro = () => {
                             <div className="Intro-Title-Item">Full-Stack Web Developer</div>
                         </div>
                     </div>
+                    <p className="Intro-Description">
+                        I am a University Student currently studying at Macquarie University, Sydney, Australia. 
+                        I am currently enrolled in a Bachelor of Engineering (Honours) with a Specialisation in Software Engineering.
+                    </p>
                 </div>
+                
             </div>
 
             <div className="Intro-Right">
-                <div className="Intro-Right-Wrapper">
-                    i right wrap
-                </div>
+                <div className="Intro-Image-Shape"></div>
+                <img src={Me} alt="photo of Mark Ghaby" className="Intro-Image" />
+                {/* <div className="Intro-Right-Wrapper">
+                    intro right wrap
+                </div> */}
             </div>
         </div>
     )
