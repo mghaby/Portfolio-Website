02e8bc19cfacfb0f72a03e318bf2383d2d55759c
mghaby <mark.ghaby71@hotmail.com>
2022-01-11 20:41:27 +1100

    placeholders and everything else is set for about me, just neeed to fill in the description

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,7 +12,13 @@ const About = () => {
             </div>
 
             <div className="About-Right">
-                
+                <h1 className="About-Title">About Me</h1>
+                <p className="About-Subtitle">Fitness Enthusiast and Gamer that loves Software Development</p>
+
+                <p className="About-Description"> talk about wanting to code as kid and thinking unachievble
+                                                losing interest through highschool and doing med stuff
+                                                then having a realisation of how i hated med and then did the bootcamp
+                                                and now a degree</p>
             </div>
         </div>
     )
