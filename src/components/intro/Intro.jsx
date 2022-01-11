bd9ab624dd71840780d7c679a5680ec24c2cec5c
mghaby <mark.ghaby71@hotmail.com>
2022-01-11 15:36:04 +1100

    glitch cleared up

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,10 +7,12 @@ const Intro = () => {
                 <div className="Intro-Left-Wrapper">
                     <h2 className="Intro-Intro">Hello, My name is</h2>
                     <h1 className="Intro-Name">Mark Ghaby.</h1>
-                    <div className="Intro-Title-Wrapper">
-                        <div className="Intro-Title-Item">Software Engineer</div>
-                        <div className="Intro-Title-Item">Blockchain Developer</div>
-                        <div className="Intro-Title-Item">Full-Stack Web Developer</div>
+                    <div className="Intro-Title">
+                        <div className="Intro-Title-Wrapper">
+                            <div className="Intro-Title-Item">Software Engineer</div>
+                            <div className="Intro-Title-Item">Blockchain Developer</div>
+                            <div className="Intro-Title-Item">Full-Stack Web Developer</div>
+                        </div>
                     </div>
                 </div>
             </div>
